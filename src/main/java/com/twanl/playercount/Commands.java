1b71f0768e755c0741ba3906d96cbde301a96d21
Twan Luttik <twanluttik@gmail.com>
2018-03-27 03:02:49 +0200

    added recover command

--- src/main/java/com/twanl/playercount/Commands.java
+++ src/main/java/com/twanl/playercount/Commands.java
@@ -1,44 +1,44 @@
 package com.twanl.playercount;
 
+import com.twanl.playercount.api.Api;
 import com.twanl.playercount.util.ConfigManager;
 import com.twanl.playercount.util.Strings;
-import net.minecraft.server.v1_12_R1.CommandExecute;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 
-public class Commands extends CommandExecute implements Listener, CommandExecutor {
+public  class Commands implements CommandExecutor, TabCompleter {
 
     private PlayerCount plugin = PlayerCount.getPlugin(PlayerCount.class);
-    public ConfigManager cfgM;
-    public File playersF;
-    public File cF;
-
-
-
+    private ConfigManager cfgM = new ConfigManager();
+    private Api api = new Api();
 
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        Player p = (Player) sender;
         if (!(sender instanceof Player)) {
             sender.sendMessage(Strings.logName + ChatColor.RED + "Only a player can execute commands!");
             return true;
         }
 
+        Player p = (Player) sender;
+        cfgM.setup();
 
 
 
-        if (cmd.getName().equalsIgnoreCase("pc")) {
+
+        if (cmd.getName().equalsIgnoreCase("playercount")) {
             if (args.length == 0) {
-                if (p.hasPermission("playercount.pc")) {
+                if (p.hasPermission("playercount")) {
                     p.sendMessage(Strings.DgrayBIS + "-----------------------------\n" +
                             Strings.goldB + "       Playercount " + plugin.getDescription().getVersion() + "\n" +
                             Strings.DgrayBIS + "-----------------------------\n" +
@@ -55,24 +55,26 @@ public class Commands extends CommandExecute implements Listener, CommandExecuto
 
             } else if (args[0].equalsIgnoreCase("reload")) {
                 if (p.hasPermission("playercount.reload")) {
-                    cF = new File(plugin.getDataFolder(), "config.yml");
+
+                    File cF = new File(plugin.getDataFolder(), "config.yml");
                     if (!cF.exists()) {
                         p.sendMessage(Strings.red + "config.yml file is not found!\n" +
                                 Strings.green + "Creating a new config.yml file!");
-                        plugin.getConfig().options().copyDefaults(true);
-                        plugin.saveConfig();
+                        plugin.saveDefaultConfig();
                     } else {
                         plugin.saveDefaultConfig();
-                        plugin.reloadConfig();
                         p.sendMessage(Strings.green + "Config File Reloaded Succsesfully!");
                     }
+
+
+
                 } else {
                     p.sendMessage(Strings.noPerm);
                 }
 
             } else if (args[0].equalsIgnoreCase("count")) {
                 if (p.hasPermission("playercount.count")) {
-                    int counts = (int) plugin.getConfig().getInt("counts");
+                    int counts = (int) cfgM.getPlayers().getInt("counts");
                     if (counts == 1) {
                         p.sendMessage(Strings.white + "There are currently " + Strings.goldB + String.valueOf(counts) + Strings.white + " players joined on this server!");
                     } else {
@@ -86,24 +88,12 @@ public class Commands extends CommandExecute implements Listener, CommandExecuto
                 if (p.hasPermission("playercount.reset")) {
                     if (plugin.getConfig().getBoolean("use_custom_file")) {
 
-
-
-
-
-
-
-
-
-
-
-
-                        playersF = new File(plugin.getDataFolder(), "PlayerData.yml");
+                        File playersF = new File(plugin.getDataFolder(), "PlayerData.yml");
                         if (playersF.exists()) {
                             playersF.delete();
                             try {
                                 playersF.createNewFile();
-                                plugin.getConfig().set("counts", 1);
-                                plugin.saveConfig();
+
                                 p.sendMessage(Strings.goldI + "Created succsesfully a new PlayerData.yml file!");
                             } catch (IOException e) {
                                 e.printStackTrace();
@@ -122,14 +112,44 @@ public class Commands extends CommandExecute implements Listener, CommandExecuto
 
 
 
+            } else if (args[0].equalsIgnoreCase("recover")) {
+                if (args.length == 1) {
+                    if (p.hasPermission("playercount.recover")) {
+
+                        if (!plugin.getConfig().getBoolean("use_custom_file")) {
+                            p.sendMessage(Strings.red + "To use this command set 'use_custom_file: true'");
+                            return true;
+                        }
+                        String confirm = " \n" +
+                                Strings.Dgreen + "[" + Strings.green + "CONFIRM" + Strings.Dgreen + "]\n" +
+                                " ";
+
+                        plugin.nms.sendClickableMessage(p, confirm, "playercount recover confirm");
+                    }
+                } else if (args[1].equalsIgnoreCase("confirm")) {
+                    for(int i=0; i < 100; i ++) {
+                        sender.sendMessage("");
+                    }
+                    api.recoverPlayers(p);
+                    p.sendMessage(Strings.green + "Succsesfully recoverd already joined players!");
+                }
             }
+
+            return true;
         }
 
 
         return true;
     }
 
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command cmd, String String, String[] args) {
+        if (args.length == 1) {
+            return Arrays.asList("reload", "count", "reset", "recover");
+        }
 
+        return null;
+    }
 
 
 }
