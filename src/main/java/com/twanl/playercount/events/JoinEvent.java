2deb2991e79e2b2843ed184c4c63a44f0f11dd65
BuildTools <unconfigured@null.spigotmc.org>
2018-07-27 03:38:27 +0200

    changed for multiple datases

--- src/main/java/com/twanl/playercount/events/JoinEvent.java
+++ src/main/java/com/twanl/playercount/events/JoinEvent.java
@@ -2,10 +2,12 @@ package com.twanl.playercount.events;
 
 
 import com.twanl.playercount.PlayerCount;
-import com.twanl.playercount.api.Api;
+import com.twanl.playercount.lib.Lib;
+import com.twanl.playercount.sql.SqlLib;
 import com.twanl.playercount.util.ConfigManager;
 import com.twanl.playercount.util.Strings;
 import com.twanl.playercount.util.UpdateChecker;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,15 +19,16 @@ public class JoinEvent implements Listener {
 
     private PlayerCount plugin = PlayerCount.getPlugin(PlayerCount.class);
     private UpdateChecker checker = new UpdateChecker(plugin);
-    private Api api = new Api();
+    private Lib api = new Lib();
     public ConfigManager cfgM;
-
 
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
 
+
+        String pluginName = plugin.getDescription().getName();
         if (plugin.getConfig().getBoolean("update_message")) {
             if (p.hasPermission("playercounts.update")) {
 
@@ -33,30 +36,45 @@ public class JoinEvent implements Listener {
                     if (checker.hasUpdate()) {
 
                         p.sendMessage(Strings.DgrayBS + "----------------------\n");
-                        plugin.nms.sendClickableHovarableMessageURL(p, Strings.red + "PlayerCounter is outdated!", Strings.gold + "Click to go to the download page", "https://www.spigotmc.org/resources/playercount.52758/");
+                        plugin.nms.sendClickableHovarableMessageURL(p, Strings.red + pluginName + " is outdated!", Strings.gold + "Click to go to the download page", "https://www.spigotmc.org/resources/playercount.52758/");
                         p.sendMessage(" \n" +
                                 Strings.white + "Your version: " + plugin.getDescription().getVersion() + "\n" +
                                 Strings.white + "Newest version: " + Strings.green + this.checker.getLatestVersion() + "\n" +
                                 Strings.DgrayBS + "----------------------");
                     } else {
                         p.sendMessage(Strings.DgrayBS + "----------------------\n" +
-                        Strings.green + "PlayerCount is up to date.\n" +
-                        Strings.DgrayBS + "----------------------");
+                                Strings.green + pluginName + " is up to date.\n" +
+                                Strings.DgrayBS + "----------------------");
                     }
                 }
             }
         }
 
 
+        // we put a 5tick delay for this becase else the player don't the join message
+        Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+            @Override
+            public void run() {
 
-        if (!plugin.getConfig().getBoolean("use_custom_file")) {
-            api.serverSide(p.getUniqueId());
-        } else {
-            api.fileSide(p.getUniqueId());
-        }
+                if (plugin.getConfig().get("database").equals("none")) {
+//                    Bukkit.getConsoleSender().sendMessage(Strings.yellow + "none");
+                    api.serverSide(p.getUniqueId());
+                } else if (plugin.getConfig().get("database").equals("file")) {
+//                    Bukkit.getConsoleSender().sendMessage(Strings.yellow + "file");
+                    api.fileSide(p.getUniqueId());
+                } else if (plugin.getConfig().get("database").equals("sql")) {
+//                    Bukkit.getConsoleSender().sendMessage(Strings.yellow + "sql");
+                    api.sql(p.getUniqueId());
+                }
+
+
+
+            }
+        }, 5L);
+
+
 
     }
-
 
 
 }
