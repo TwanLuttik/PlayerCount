da3aa6b068092afd2b059caa03368f15bb66a2fa
BuildTools <unconfigured@null.spigotmc.org>
2018-07-27 03:39:45 +0200

    changed for faster coding

--- src/main/java/com/twanl/playercount/util/UpdateChecker.java
+++ src/main/java/com/twanl/playercount/util/UpdateChecker.java
@@ -21,10 +21,11 @@ public class UpdateChecker {
     public String getLatestVersion() {
         try {
             int resource = 52758;
-            HttpURLConnection con = (HttpURLConnection)new URL("https://api.spigotmc.org/legacy/update.php?resource=52758").openConnection();
+            HttpURLConnection con = (HttpURLConnection) new URL("https://api.spigotmc.org/legacy/update.php?resource=" + resource).openConnection();
             con.setDoOutput(true);
             con.setRequestMethod("POST");
-            con.getOutputStream().write("key=98BE0FE67F88AB82B4C197FAF1DC3B69206EFDCC4D3B80FC83A00037510B99B4&resource=52758".getBytes("UTF-8"));
+            String key = "key=98BE0FE67F88AB82B4C197FAF1DC3B69206EFDCC4D3B80FC83A00037510B99B4&resource=" + resource;
+            con.getOutputStream().write(key.getBytes("UTF-8"));
             String version = new BufferedReader(new InputStreamReader(con.getInputStream())).readLine();
             if (version.length() <= 7) {
                 return version;
